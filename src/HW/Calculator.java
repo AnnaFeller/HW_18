977c26a38fcdde021338cc90007e2e4aeb8e822c
Anna_F <felleranna31@gmail.com>
2026-02-28 22:20:16 +0200

    hw

--- src/HW/Calculator.java
+++ src/HW/Calculator.java
@@ -8,7 +8,7 @@ import java.util.function.BinaryOperator;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println(mapOperation.get("-").apply(8,6));
+        System.out.println(mapOperation.get("-").apply(-8,6));
     }
     public static Map<String, BinaryOperator<Integer>> mapOperation;
     static {
@@ -19,25 +19,38 @@ public class Calculator {
         mapOperation.put("/", (a, b) -> a / b);
     }
 
-    public static Integer computerExpression(String str){
-        if(!str.matches("\\d+([+\\-*/])\\d+")){
+    public static Integer computeExpression(String str) {
+
+        str = str.replaceAll("\\s+", "");
+
+        if (!str.matches("-?\\d+([+\\-*/]-?\\d+)*")) {
             throw new IllegalArgumentException("error");
         }
+
         String[] numbers = str.split("[+\\-*/]");
 
         List<String> operators = new ArrayList<>();
-        for(char c : str.toCharArray()){
-            if("+-*/".indexOf(c)>=0){
+        for (char c : str.toCharArray()) {
+            if ("+-*/".indexOf(c) >= 0) {
                 operators.add(String.valueOf(c));
             }
         }
+
         int res = Integer.parseInt(numbers[0]);
-for (int i = 0;i<operators.size();i++){
-    String operator = operators.get(i);
-    int nextNum = Integer.parseInt(numbers[i+1]);
-    BinaryOperator<Integer> op = mapOperation.get(operator);
-    res = op.apply(res,nextNum);
-}
-return res;
+
+        for (int i = 0; i < operators.size(); i++) {
+            String operator = operators.get(i);
+            int nextNumber = Integer.parseInt(numbers[i + 1]);
+
+            if (operator.equals("/") && nextNumber == 0) {
+                throw new ArithmeticException("division by 0");
+            }
+
+            BinaryOperator<Integer> op = mapOperation.get(operator);
+            res = op.apply(res, nextNumber);
+        }
+
+        return res;
     }
+
 }
