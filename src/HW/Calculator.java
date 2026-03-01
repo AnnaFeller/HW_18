1800adc69ed7372e1ac27c01571fd121ee3b46fd
Anna_F <felleranna31@gmail.com>
2026-03-01 18:51:12 +0200

    hw

--- src/HW/Calculator.java
+++ src/HW/Calculator.java
@@ -5,52 +5,56 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BinaryOperator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
-    public static void main(String[] args) {
-        System.out.println(mapOperation.get("-").apply(-8,6));
-    }
-    public static Map<String, BinaryOperator<Integer>> mapOperation;
+    public static Map<String, BinaryOperator<Integer>> mapOperations;
+
     static {
-        mapOperation = new HashMap<>();
-        mapOperation.put("+", (a, b) -> a + b);
-        mapOperation.put("-", (a, b) -> a - b);
-        mapOperation.put("*", (a, b) -> a * b);
-        mapOperation.put("/", (a, b) -> a / b);
+        mapOperations = new HashMap<>();
+        mapOperations.put("+", (a, b) -> a + b);
+        mapOperations.put("-", (a, b) -> a - b);
+        mapOperations.put("*", (a, b) -> a * b);
+        mapOperations.put("/", (a, b) -> a / b);
     }
 
     public static Integer computeExpression(String str) {
-
         str = str.replaceAll("\\s+", "");
 
         if (!str.matches("-?\\d+([+\\-*/]-?\\d+)*")) {
             throw new IllegalArgumentException("error");
         }
 
-        String[] numbers = str.split("[+\\-*/]");
+        List<Integer> nums = new ArrayList<>();
+        List<String> ops = new ArrayList<>();
 
-        List<String> operators = new ArrayList<>();
-        for (char c : str.toCharArray()) {
-            if ("+-*/".indexOf(c) >= 0) {
-                operators.add(String.valueOf(c));
+        Matcher m = Pattern.compile("-?\\d+").matcher(str);
+        while (m.find()) nums.add(Integer.parseInt(m.group()));
+
+        Matcher mOp = Pattern.compile("(?<=\\d)[+\\-*/]").matcher(str);
+        while (mOp.find()) ops.add(mOp.group());
+
+        for (int i = 0; i < ops.size(); i++) {
+            String op = ops.get(i);
+            if (op.equals("*") || op.equals("/")) {
+                int res = mapOperations.get(op).apply(nums.get(i), nums.get(i + 1));
+                nums.set(i, res);
+                nums.remove(i + 1);
+                ops.remove(i);
+                i--;
             }
         }
 
-        int res = Integer.parseInt(numbers[0]);
-
-        for (int i = 0; i < operators.size(); i++) {
-            String operator = operators.get(i);
-            int nextNumber = Integer.parseInt(numbers[i + 1]);
-
-            if (operator.equals("/") && nextNumber == 0) {
-                throw new ArithmeticException("division by 0");
-            }
-
-            BinaryOperator<Integer> op = mapOperation.get(operator);
-            res = op.apply(res, nextNumber);
+        int finalRes = nums.get(0);
+        for (int i = 0; i < ops.size(); i++) {
+            finalRes = mapOperations.get(ops.get(i)).apply(finalRes, nums.get(i + 1));
         }
 
-        return res;
+        return finalRes;
     }
 
+    public static void main(String[] args) {
+        System.out.println(computeExpression("34-2*5/4"));
+    }
 }
